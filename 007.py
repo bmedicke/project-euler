0a45a8f60618f3e4edb2c6b98ec713c698dfe0ca
Benjamin Medicke <bmedicke@gmail.com>
2018-04-09 21:30:43 +0200

    solved 007

---
+++ 007.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+"""
+
+i = 2
+prime_counter = 1
+
+while prime_counter <= 10_001:
+    prime = True
+    for n in range(2, i):
+        if i % n == 0:
+            prime = False
+            break
+    if prime:
+        print(prime_counter, ':', i)
+        prime_counter += 1
+    i += 1
